772c17ffbe3adf18309f0b9cc8761168187fef94
account3r2 <ilove1000bananas@gmail.com>
2015-02-09 17:55:59 -0800

    Added interface.

--- potatocounter.cpp
+++ potatocounter.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 #include <unistd.h>
 
+int maxPotatoes;
+
 int main() {
 	std::cout << "Potato.\n";
 
 	usleep(1000000);
 
-	for (int i = 1; i <= 100; i++) {
+	std::cout << "How many potatoes should we count to?\n> ";
+	std::cin >> maxPotatoes;
+	std::cout << "Counting to " << maxPotatoes << " potatoes.\n";
+
+	usleep(1000000);
+
+	for (int i = 1; i <= maxPotatoes; i++) {
+		std::cout << i << " potatoes.\n";
 		usleep(250000);
-		std::cout << i << std::endl;
 	}
+
+	std::cout << "ALL of the potatoes!\n";
 
 	return 0;
 }
